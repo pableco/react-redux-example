38b89e64d7e59d7e6d7f93172b3adf674191efa2
Pablo Grillo <pablo.grillo@roiback.com>
2021-04-05 20:11:57 +0200

    feat(optimization): add lazy load images

--- src/component/Item/Item.js
+++ src/component/Item/Item.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 import { deleteItem } from '../../redux/actions/apiActions';
 import Button from '../../commonStyles/Buttons.styles';
@@ -9,7 +10,10 @@ const Item = ({data, deleteItem}) => {
     return (
         <li id={data.id}>
             <h5>{data.name}</h5>
-            <img alt={data.name} src={data.imageUrl} />
+            <LazyLoadImage
+                alt={data.name}
+                src={data.imageUrl}
+                />
             <Link to={`/edit/${data.id}`}>
                 <Button.Invert>
                     Edit
